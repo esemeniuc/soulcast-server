b81698afc6017684cd8d3c0362c2dad80ff6742e
cwaffles <cwaffles@users.noreply.github.com>
2016-09-04 16:52:44 -0700

    no sudo in script

--- setup-ubuntu-server.sh
+++ setup-ubuntu-server.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y nodejs
 
 ###this installs RVM and Rails
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
 source ~/.rvm/scripts/rvm
 gem install rails
 
