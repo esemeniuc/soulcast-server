61264c7151a8fdfe229a09cb424258138400779f
cwaffles <cwaffles@users.noreply.github.com>
2016-10-08 20:50:50 -0700

    remove need for nearby controller

--- app/controllers/nearby_controller.rb
+++
@@ -1,9 +0,0 @@
-class NearbyController < ApplicationController
-
-  # GET /nearby/SOME_ID_HERE.json
-  def fetch
-    randInt = Random.new
-    jsonReturn = {nearby: randInt.rand(20)}
-    render :json => jsonReturn
-  end
-end
--- test/controllers/nearby_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class NearbyControllerTest < ActionDispatch::IntegrationTest
-  test "should get fetch" do
-    get nearby_fetch_url
-    assert_response :success
-  end
-
-end
