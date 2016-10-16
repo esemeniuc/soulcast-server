303f1fface12333f6e3b9dec4f0ea3b7960e7397
cwaffles <cwaffles@users.noreply.github.com>
2016-10-16 13:49:30 -0700

    add website controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'website#index' #for production mode
+
   resources :devices
   resources :souls
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
