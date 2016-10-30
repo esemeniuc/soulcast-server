99c0d97bdaa7d9931c5a416be0e257b7d52fef90
cwaffles <cwaffles@users.noreply.github.com>
2016-10-29 18:19:06 -0700

    migrate to postgres2

--- setup-ubuntu-server.sh
+++ setup-ubuntu-server.sh
@@ -16,6 +16,7 @@ sudo apt-get install curl
 ###install nodejs (required for rails)
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo npm install dotenv
 
 ###install and setup nginx
 sudo add-apt-repository ppa:nginx/stable
@@ -27,6 +28,12 @@ sudo wget $nginx_config -O /etc/nginx/sites-available/soulcast.ml.conf
 sudo ln -s /etc/nginx/sites-available/soulcast.ml.conf /etc/nginx/sites-enabled/soulcast.ml.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx start
+
+###install postgres #libpq-dev for pg gem
+sudo apt-get install postgresql libpq-dev
+sudo -u postgres createuser -s soulcast-dev
+sudo -u postgres psql
+echo "\password soulcast-dev\nsoulcast-dev\nsoulcast-dev\n\q\n"
 
 ###this installs RVM and Rails
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
