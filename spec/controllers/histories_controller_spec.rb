eaa7a858bfc819591f2f0cca075f6f0d962a674e
Kevin Zhang <kevinctzhang@gmail.com>
2016-12-27 15:28:50 -0800

    history testing through

--- spec/controllers/histories_controller_spec.rb
+++ spec/controllers/histories_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe HistoriesController, type: :controller do
-	context 'when device had already received souls' do
+	context 'when one device had already received souls' do
 		xit 'one souls received'do
 			#expect(device1.history)=one soul
 		end
@@ -10,6 +10,25 @@ RSpec.describe HistoriesController, type: :controller do
 		end
 		xit 'three souls received'do
 			#expect(device1.history)=three souls
+		end
+	end
+	context 'when one device had NOT received any souls' do
+		xit 'No souls received' do
+			#expect(device1.history)= nosouls
+		end
+	end
+	context 'when two devices had already received souls' do
+		xit 'one souls received'do
+			#expect(device1.history)=one soul
+			#expect(device2.history)=one soul
+		end
+		xit 'two souls received'do
+			#expect(device1.history)=two souls
+			#expect(device2.history)=two souls
+		end
+		xit 'three souls received'do
+			#expect(device1.history)=three souls
+			#expect(device2.history)=thres souls
 		end
 	end
 
