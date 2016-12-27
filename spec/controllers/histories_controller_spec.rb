e160685ff470cb127772f3e7c0d477e8c7cf01f6
Kevin Zhang <kevinctzhang@gmail.com>
2016-12-27 15:19:25 -0800

    histories spec added

---
+++ spec/controllers/histories_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe HistoriesController, type: :controller do
+	context 'when device had already received souls' do
+		xit 'one souls received'do
+			#expect(device1.history)=one soul
+		end
+		xit 'two souls received'do
+			#expect(device1.history)=two souls
+		end
+		xit 'three souls received'do
+			#expect(device1.history)=three souls
+		end
+	end
+
+end
