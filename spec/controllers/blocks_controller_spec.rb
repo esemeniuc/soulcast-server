94206eb7ba6d8be98bd03c19094f6e86b6cec5be
Kevin Zhang <kevinctzhang@gmail.com>
2016-12-27 16:41:10 -0800

    blocks controller spec

---
+++ spec/controllers/blocks_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe BlocksController, type: :controller do
+	context 'when device1 blocks device2' do
+		xit 'should have successfully blocked device2' do
+			#expect(device1.blocks.contains)=device2
+			#expect(device2.beingblocked.contains)=device1
+		end
+	end
+	context 'when device1 did NOT block device2' do
+		xit 'should NOT have blocked device2' do
+			#expect(device1.blocks.contains)= no device2
+			#expect(device2.beingblocked.contains)= no device1
+		end
+	end
+	context 'when device1 blocks device2 but not device3' do
+		xit 'should have blocked device2' do
+			#expect(device1.blocks.contains)= device2
+			#expect(device2.beingblocked.contains)= device1
+		end
+		xit 'should NOT have blocked device3' do
+			#expect(device1.blocks.contains)= no device3
+			#expect(device3.beingblocked.contains)= no device1
+		end
+	end
+end
