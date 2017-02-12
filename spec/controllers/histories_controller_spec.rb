542f213c5d1d636dec5e28c08e4e494fe88a495f
James Leong <jyleong@sfu.ca>
2017-02-12 14:44:53 -0800

    fix rspec test for blocking bug

--- spec/controllers/histories_controller_spec.rb
+++ spec/controllers/histories_controller_spec.rb
@@ -141,7 +141,7 @@ RSpec.describe HistoriesController, type: :controller do
   context "blocking after sending a soul" do
     # dev1 sends a soul to all nearby, dev1 is later blocked by dev2 and dev5
     # dev1 is reachable by dev2, dev5, and dev6
-    before(:all) do
+    before(:each) do
       @soul = Soul.create(soulType: "testType1",
                          s3Key: 10000000,
                          epoch: 1000000,
@@ -203,7 +203,7 @@ RSpec.describe HistoriesController, type: :controller do
                      radius: 20,
                      token: @dev2.token,
                      device_id: @dev2.id)
-
+      # binding.pry
       expect(@dev2.histories.count).to eq(1)
       expect(@dev1.histories.count).to eq(1)
       Block.create(blocker_id: @dev2.id, blockee_id: @dev1.id)
