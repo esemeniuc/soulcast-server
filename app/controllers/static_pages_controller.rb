7a24dbef9a41e43d9a6d125145e02c30dd72235d
cwaffles <cwaffles@users.noreply.github.com>
2017-02-13 00:19:51 -0800

    migrate static page serving over to rails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   def index
+    @page_title = 'Soulcast | Send a message from your soul'
+    @description = nil
   end
 
   def eula
+    @page_title = 'Soulcast EULA'
+    @description = nil
   end
 end
