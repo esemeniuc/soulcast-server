74dee636c5e003116505370dfb42e6120b99a554
cwaffles <cwaffles@users.noreply.github.com>
2017-02-13 00:19:51 -0800

    migrate static page serving over to rails

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 # Custom stuff down here
+gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'geocoder'
 gem 'dotenv'
 gem 'dotenv-rails'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+controllers = Dir['app/controllers/*.rb'].map {|f| File.basename(f, ".rb").chomp("_controller")} #used for custom css loading
+controllers.each do |controller|
+  Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css"]
+end
